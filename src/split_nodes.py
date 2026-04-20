39d2e33a6756ced582428f21a41729ce0ff60b9d
al3bdzo <abdalrhmanmohammed307@gmail.com>
2026-04-20 15:21:57 +0200

    splitting images and links

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,3 +1,5 @@
+import re
+
 from .textnode import TextNode, TextType
 
 
@@ -22,4 +24,65 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_nodes.append(TextNode(node_text[i], TextType.TEXT))
     
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+        if node.text_type != TextType.TEXT or len(images) == 0:
+            new_nodes.append(node)
+            continue
+
+        node_text = node.text
+        for image in images:
+            sections = node_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown syntax")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+            node_text = sections[1]
+
+        if node_text != "":
+            new_nodes.append(TextNode(node_text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+        if node.text_type != TextType.TEXT or len(links) == 0:
+            new_nodes.append(node)
+            continue
+
+        node_text = node.text
+        for link in links:
+            sections = node_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown syntax")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            node_text = sections[1]
+
+        if node_text != "":
+            new_nodes.append(TextNode(node_text, TextType.TEXT))
+
     return new_nodes
